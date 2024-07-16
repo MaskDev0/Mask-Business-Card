90cbb72a4263372e3ab0644eb6684c2c4f7458b6
Wisdom Ojonugwa <maskdev0@gmail.com>
2024-07-16 12:52:24 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import { faGithub, faSquareInstagram, faLinkedin ,faTwitter} from '@fortawesome/
 function Footer() {
   return (
     <div className="footer-container">
-      <a href="github.com/MaskDev0"><FontAwesomeIcon icon={faGithub} /></a>
+      <a href="https://github.com/MaskDev0"><FontAwesomeIcon icon={faGithub} /></a>
       <a href="https://www.instagram.com/mask_develops/"> <FontAwesomeIcon icon={faSquareInstagram} /></a>
       <a href="https://www.linkedin.com/in/wisdom-ojonugwa-a080772ab?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FontAwesomeIcon icon={faLinkedin} /></a>
       <a href="https://x.com/MaskDevelops"><FontAwesomeIcon icon={faTwitter} /></a>
